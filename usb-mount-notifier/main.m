afe0e94b4d4e0bb7dce143086486eedbe351c75c
Aleksandr Kunin <ab@ipkunin.ru>
2015-11-11 21:02:49 +0300

    Initial Commit

---
+++ usb-mount-notifier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  usb-mount-notifier
+//
+//  Created by Александр Кунин on 11.11.15.
+//  Copyright © 2015 Skydebrby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
