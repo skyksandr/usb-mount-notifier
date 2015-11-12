5f99304b9b50796071d4dd13d191150f23fd41af
Aleksandr Kunin <ab@ipkunin.ru>
2015-11-12 11:38:02 +0300

    printf replaced with write to stdout, added license

--- src/Notifier.h
+++ src/Notifier.h
@@ -1,16 +1,32 @@
+//  Copyright © 2015 Aleksandr Kunin
 //
-//  Notifier.h
-//  usb-mount-notifier
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
 //
-//  Created by Александр Кунин on 11.11.15.
-//  Copyright © 2015 Skydebrby. All rights reserved.
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
 //
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #ifndef Notifier_h
 #define Notifier_h
 
 @interface Notifier : NSObject
-    
+
+// write string to stdout
+- (void)writeToSTDOUT:(NSString *)format, ...;
+
 // enter this method when a volume is mounted
 - (void)didMountMethod:(NSNotification *)notification;
 // enter this method when a volume is unmounted
--- src/Notifier.m
+++ src/Notifier.m
@@ -1,10 +1,23 @@
+//  Copyright © 2015 Aleksandr Kunin
 //
-//  Notifier.m
-//  usb-mount-notifier
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
 //
-//  Created by Александр Кунин on 11.11.15.
-//  Copyright © 2015 Skydebrby. All rights reserved.
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
 //
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
@@ -13,14 +26,28 @@
 
 @implementation Notifier : NSObject
 
+
+- (void)writeToSTDOUT:(NSString *)format, ...
+{
+    va_list args;
+    va_start(args, format);
+    NSString *formattedString = [[NSString alloc] initWithFormat: format
+                                                       arguments: args];
+    va_end(args);
+    [[NSFileHandle fileHandleWithStandardOutput]
+     writeData: [formattedString dataUsingEncoding: NSNEXTSTEPStringEncoding]];
+    [formattedString release];
+}
+
 - (void)didMountMethod:(NSNotification *)notification
 {
-    printf("%s%s\n", "mounted: ", [[notification userInfo][@"NSDevicePath"] UTF8String]);
+    [self writeToSTDOUT:@"%s%s\n", "mounted: ", [[notification userInfo][@"NSDevicePath"] UTF8String]];
 }
 
 - (void)didUnmountMethod:(NSNotification *)notification
 {
-    printf("%s%s\n", "unmounted: ", [[notification userInfo][@"NSDevicePath"] UTF8String]);
+    [self writeToSTDOUT:@"%s%s\n", "unmounted: ", [[notification userInfo][@"NSDevicePath"] UTF8String]];
 }
+
 
 @end
